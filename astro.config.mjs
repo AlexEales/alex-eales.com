8b8c41e2ded5c18e3b31adc567edb19b78c17af4
Alex Eales <dev@alex-eales.com>
2022-11-09 20:38:13 +0000

    Added typing title

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()]
 });
---
+++ src/components/Landing.svelte
@@ -0,0 +1,44 @@
+<div class="flex flex-col items-center justify-center h-screen">
+    <div class="container">
+        <h1 class="type-out font-black text-9xl">
+            Alex Eales
+        </h1>
+        <p class="text-xl">
+            Backend Engineer
+        </p>
+    </div>
+</div>
+
+<style>
+    @keyframes typing {
+        0% {
+            padding-right: 0;
+            width: 0;
+        }
+        15% {
+            padding-right: 0.1em;
+            width: 100%;
+        }
+        85% {
+            padding-right: 0.1em;
+            width: 100%;
+        }
+        100% {
+            padding-right: 0;
+            width: 0;
+        }
+    }
+
+    .container {
+        width: fit-content;
+    }
+
+    .type-out {
+        animation: typing 8s alternate infinite;
+        border-right: .15em solid;
+        overflow: hidden;
+        padding-right: 0.1em;
+        white-space: nowrap;
+        width: 0;
+    }
+</style>
